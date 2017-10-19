8bebec4261e74a855f564ffaf788865e25db2820
jissjoseph82 <jissjoseph82@gmail.com>
2017-10-19 11:14:45 +0530

    Update postgrator.js

--- postgrator.js
+++ postgrator.js
@@ -14,14 +14,14 @@ postgrator.setConfig({
   connectionString: process.env.DATABASE_URL,
 });
 
-/*postgrator.setConfig({
+postgrator.setConfig({
   migrationDirectory: __dirname + '/migrations',
   driver: 'pg', // or mysql, mssql
   host: process.env.RDS_HOSTNAME,
   port: process.env.RDS_PORT, // optionally provide port
   username: process.env.RDS_USERNAME,
   password: process.env.RDS_PASSWORD
-});*/
+});
 
 // migrate to version specified, or supply 'max' to go all the way up
 postgrator.migrate('max', function(err, migrations) {
